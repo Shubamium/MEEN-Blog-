735ef90ca43db3b68ad5f7be6621024805caa2a2
Vincent Liussyaputra <liusvinv@gmail.com>
2023-07-02 13:52:55 +0700

    Add index and blog router

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require("express");
-
 const app = express();
 
+// Controller
+const blogRoutes = require("./routes/blog");
+
+// app.set('view engine', 'ejs');
+// Start the server
 app.listen(3000, () => {
   console.log("Server Started at port 3000");
 });
+
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("index");
+});
+app.use("/blog", blogRoutes);
+
+app.use((req, res) => {
+  //   res.render;
 });
