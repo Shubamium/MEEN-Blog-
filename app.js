b9c502880f750f56a0ab1520263880ca35e0c8d7
Vincent Liussyaputra <liusvinv@gmail.com>
2023-07-02 14:05:59 +0700

    Add EJS and basics partials

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const app = express();
 // Controller
 const blogRoutes = require("./routes/blog");
 
-// app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 // Start the server
 app.listen(3000, () => {
   console.log("Server Started at port 3000");
 });
 
 app.get("/", (req, res) => {
-  res.send("index");
+  res.render("index");
 });
 app.use("/blog", blogRoutes);
 
