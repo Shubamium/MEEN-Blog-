e6e43692522d00ad9479d70addac6ce6e561bbba
Vincent Liussyaputra <liusvinv@gmail.com>
2023-07-02 14:15:32 +0700

    Add footer partials and base page template

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const app = express();
 
 // Controller
 const blogRoutes = require("./routes/blog");
+const morgan = require("morgan");
 
+// Middleware
 app.set("view engine", "ejs");
+app.use(morgan("dev"));
+
 // Start the server
 app.listen(3000, () => {
   console.log("Server Started at port 3000");
